a8945511ff96d52e1ee4c9dabbedfd7cc191dc84
Octane0411 <wdznb1@gmail.com>
2022-02-27 12:12:25 +0800

    重构main

--- main.go
+++ main.go
@@ -10,27 +10,32 @@ import (
 ) //勾
 
 func main() {
-	//启动gin服务
 	go server.Run()
 
 	//var ui lorca.UI
 	//ui, _ = lorca.New("https://baidu.com", "", 800, 600, "--disable-sync", "--disable-translate")
 
-	//先写死路径，后面再改
-	//启动chrome
+	cmd := startBrowser()
+	chSignal := listenToInterrupt(cmd)
+	select {
+	case <-chSignal:
+		cmd.Process.Kill()
+	}
+
+}
+
+func startBrowser() *exec.Cmd {
 	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 	cmd := exec.Command(chromePath, "http://127.0.0.1:27149/static/index.html")
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return cmd
+}
 
-	//监听中断信号
+func listenToInterrupt(cmd *exec.Cmd) chan os.Signal {
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
-	}
-
+	return chSignal
 }
